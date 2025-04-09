f4fffa8f8c8e1299cf5b41b7bd124c1e9c0bd62e
zk <ciminuv@gmail.com>
2025-04-09 21:23:32 +0700

    export error types

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { Chain, Address } from "viem";
 export * as chains from "viem/chains";
 export { Vault, encode, decode } from "@zkpay/core";
 
+export * from "./lib/errors";
 export * from "./lib/utils";
 export * from "./lib/types";
 export * from "./lib/merchant";
